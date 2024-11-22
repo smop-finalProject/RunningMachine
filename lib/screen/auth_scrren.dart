56ca019f3f205586ab5f1a82772453f901cf447c
정태민 <jtmexs1218@jeongtaemin-ui-MacBookAir.local>
2024-11-22 20:22:00 +0900

    새로연결

--- lib/screen/auth_scrren.dart
+++ lib/screen/auth_scrren.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smop_final/screen/root_screen.dart';
 
+
 import 'auth_home.dart';
 
 class Auth extends StatelessWidget{
